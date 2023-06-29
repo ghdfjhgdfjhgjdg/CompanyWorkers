86197025cfe3f524a18dbc821d951c73f555f5aa
ghdfjhgdfjhgjdg <sounddummies5@gmail.com>
2023-06-29 19:25:34 +0300

    Add dict to project

---
+++ main.py
@@ -0,0 +1,27 @@
+employees = {
+    "Jonh": {
+        "position": "developer",
+        "salary": 25000,
+        "start_date": "2023-01-01"
+    },
+    "Alex": {
+        "position": "manager",
+        "salary": 15000,
+        "start_date": "2023-01-01"
+    },
+    "Cat": {
+        "position": "manager",
+        "salary": 15000,
+        "start_date": "2023-01-01"
+    },
+    "Fredo": {
+        "position": "cleaner",
+        "salary": 5000,
+        "start_date": "2023-01-01"
+    },
+    "Merphy": {
+        "position": "doctor",
+        "salary": 10000,
+        "start_date": "2023-01-01"
+    }
+}
