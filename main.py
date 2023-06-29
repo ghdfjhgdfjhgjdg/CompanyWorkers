ed965aecbdf5b9c22e11c3f423da88adb78dc41e
Vadim <vadimpapusha2310@gmail.com>
2023-06-29 19:58:26 +0300

    added add and remove function

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ employees = {
     },
     "Fredo": {
         "position": "cleaner",
-        "salary": 5000,
+        "salary": 6000,
         "start_date": "2023-01-01"
     },
     "Merphy": {
@@ -25,3 +25,37 @@ employees = {
         "start_date": "2023-01-01"
     }
 }
+
+while True:
+    get_info = input('Введіть команду: ')
+    if get_info == 'add':
+
+        add_worker_name = input("Введіть ім'я: ")
+        if add_worker_name == '' or add_worker_name == 'quit':
+            break
+        add_worker_position = input("Введіть позицію вашого працівника: ")
+        add_worker_salary = input("Введіть зарплату вашого працівника: ")
+        add_worker_start_date = input("Введіть дату початку роботи вашого працівника: ")
+        employees[add_worker_name] = {
+            'position': add_worker_position,
+            'salary': add_worker_salary,
+            'start_date': add_worker_start_date
+        }
+
+    if get_info == 'remove':
+        while True:
+            del_item_name = input('Введіть імя для видалення: ')
+            if del_item_name in employees:
+                del employees[del_item_name]
+                print('Користувача не знайдено!')
+                break
+    print(employees)
+
+
+
+
+
+
+
+
+
